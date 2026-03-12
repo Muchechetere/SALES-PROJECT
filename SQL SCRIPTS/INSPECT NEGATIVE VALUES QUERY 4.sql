d33fdb8068259019440129c84dfb9357e822dc57
tutsirai <moyotutsirai@gmail.com>
2026-03-12 18:36:00 +0000

    Inspecting negative values

---
+++ SQL SCRIPTS/INSPECT NEGATIVE VALUES QUERY 4.sql
@@ -0,0 +1,9 @@
+-- Negative & 0 values
+SELECT 
+    'Negative' AS [Value Type],
+    [Sell Region],
+    [Sell Sub-Region],
+    [Fiscal Month],
+    [Value]
+FROM [dbo].[SalesFigures_FY18]-- Checking for zero or negative values in the SalesFigures_FY__ table(can alternate between 18 to 22)
+WHERE [Value] <= 0
