814b43b655d4d05dc27ae1583742aed1652a550e
tutsirai <moyotutsirai@gmail.com>
2026-03-30 17:32:55 +0100

    Create DELETE ZERO VALUES QUERY 11.sql

---
+++ SQL SCRIPTS/DELETE ZERO VALUES QUERY 11.sql
@@ -0,0 +1,5 @@
+DELETE FROM SalesFigures_FY18 WHERE [Value] = 0;
+DELETE FROM SalesFigures_FY19 WHERE [Value] = 0;
+DELETE FROM SalesFigures_FY20 WHERE [Value] = 0;
+DELETE FROM SalesFigures_FY21 WHERE [Value] = 0;
+DELETE FROM SalesFigures_FY22 WHERE [Value] = 0;
