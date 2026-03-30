9ca17be462264eb52417a089ad92c6d03ed23694
tutsirai <moyotutsirai@gmail.com>
2026-03-30 16:30:14 +0100

    Create EDIT COMBINED VIEW QUERY 10.sql

---
+++ SQL SCRIPTS/EDIT COMBINED VIEW QUERY 10.sql
@@ -0,0 +1,45 @@
+CREATE OR ALTER VIEW dbo.vw_AllSalesFigures
+AS
+SELECT 
+    [Sell Region], [Sell Sub-Region], [Fiscal Month], 
+    CAST(ISNULL([Value], 0) AS DECIMAL(18,2)) AS [Value], 
+    'FY18' AS [Fiscal Year]
+FROM dbo.SalesFigures_FY18
+WHERE ROUND(ISNULL([Value], 0), 4) <> 0
+
+UNION ALL
+
+SELECT 
+    [Sell Region], [Sell Sub-Region], [Fiscal Month], 
+    CAST(ISNULL([Value], 0) AS DECIMAL(18,2)) AS [Value], 
+    'FY19' AS [Fiscal Year]
+FROM dbo.SalesFigures_FY19
+WHERE ROUND(ISNULL([Value], 0), 4) <> 0
+
+UNION ALL
+
+SELECT 
+    [Sell Region], [Sell Sub-Region], [Fiscal Month], 
+    CAST(ISNULL([Value], 0) AS DECIMAL(18,2)) AS [Value], 
+    'FY20' AS [Fiscal Year]
+FROM dbo.SalesFigures_FY20
+WHERE ROUND(ISNULL([Value], 0), 4) <> 0
+
+UNION ALL
+
+SELECT 
+    [Sell Region], [Sell Sub-Region], [Fiscal Month], 
+    CAST(ISNULL([Value], 0) AS DECIMAL(18,2)) AS [Value], 
+    'FY21' AS [Fiscal Year]
+FROM dbo.SalesFigures_FY21
+WHERE ROUND(ISNULL([Value], 0), 4) <> 0
+
+UNION ALL
+
+SELECT 
+    [Sell Region], [Sell Sub-Region], [Fiscal Month], 
+    CAST(ISNULL([Value], 0) AS DECIMAL(18,2)) AS [Value], 
+    'FY22' AS [Fiscal Year]
+FROM dbo.SalesFigures_FY22
+WHERE ROUND(ISNULL([Value], 0), 4) <> 0;
+GO
