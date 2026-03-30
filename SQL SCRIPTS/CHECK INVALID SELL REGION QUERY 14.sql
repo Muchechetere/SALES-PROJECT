3ceff1ec83c2b8ae0c615e07cc51aca8fb414de3
tutsirai <moyotutsirai@gmail.com>
2026-03-30 20:56:35 +0100

    Update CHECK INVALID SELL REGION QUERY 14.sql

--- SQL SCRIPTS/CHECK INVALID SELL REGION QUERY 14.sql
+++ SQL SCRIPTS/CHECK INVALID SELL REGION QUERY 14.sql
@@ -1,5 +1,5 @@
 
 SELECT DISTINCT [Sell Region]
-FROM [dbo].[SalesFigures_FY22]--checking invalid sell regions in the latest fiscal year
+FROM [dbo].[SalesFigures_FY18]--checking invalid sell regions in the latest fiscal year
 WHERE [Sell Region] IS NOT NULL
 ORDER BY [Sell Region];
