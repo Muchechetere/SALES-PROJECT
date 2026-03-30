865934a7195add98083cba7fe5a3897cf213743d
tutsirai <moyotutsirai@gmail.com>
2026-03-30 21:11:51 +0100

    Update CREATE MONTH DIMENSION VIEW QUERY 8 .sql

--- SQL SCRIPTS/CREATE MONTH DIMENSION VIEW QUERY 8 .sql
+++ SQL SCRIPTS/CREATE MONTH DIMENSION VIEW QUERY 8 .sql
@@ -1,6 +1,6 @@
 CREATE VIEW vw_Dim_FiscalMonth AS
 SELECT DISTINCT
-    [Fiscal_Year],
+    [Fiscal Year],
     [Fiscal Month],
     CAST(RIGHT([Fiscal Month], 2) AS INT) AS Month_Number
 FROM vw_SalesFigures_All;
