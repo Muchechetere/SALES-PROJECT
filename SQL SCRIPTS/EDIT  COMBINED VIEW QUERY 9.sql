00eb271a2de82594f4da41139ece8ffbff383873
tutsirai <moyotutsirai@gmail.com>
2026-03-30 16:16:35 +0100

    Create EDIT  COMBINED VIEW QUERY 9.sql

---
+++ SQL SCRIPTS/EDIT  COMBINED VIEW QUERY 9.sql
@@ -0,0 +1,35 @@
+CREATE OR ALTER VIEW dbo.vw_AllSalesFigures
+AS
+SELECT 
+    [Sell Region], [Sell Sub-Region], [Fiscal Month], [Value], 'FY18' AS [Fiscal Year]
+FROM dbo.SalesFigures_FY18
+WHERE ISNULL([Value], 0) <> 0
+
+UNION ALL
+
+SELECT 
+    [Sell Region], [Sell Sub-Region], [Fiscal Month], [Value], 'FY19' AS [Fiscal Year]
+FROM dbo.SalesFigures_FY19
+WHERE ISNULL([Value], 0) <> 0
+
+UNION ALL
+
+SELECT 
+    [Sell Region], [Sell Sub-Region], [Fiscal Month], [Value], 'FY20' AS [Fiscal Year]
+FROM dbo.SalesFigures_FY20
+WHERE ISNULL([Value], 0) <> 0
+
+UNION ALL
+
+SELECT 
+    [Sell Region], [Sell Sub-Region], [Fiscal Month], [Value], 'FY21' AS [Fiscal Year]
+FROM dbo.SalesFigures_FY21
+WHERE ISNULL([Value], 0) <> 0
+
+UNION ALL
+
+SELECT 
+    [Sell Region], [Sell Sub-Region], [Fiscal Month], [Value], 'FY22' AS [Fiscal Year]
+FROM dbo.SalesFigures_FY22
+WHERE ISNULL([Value], 0) <> 0;
+GO
